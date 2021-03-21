0f49c50bab5bcdf27d94ca8cbb51737a2b4d71be
F4bioo <costa.fbo@gmail.com>
2021-03-21 00:26:55 +0000

    Set Gradle dependencies and UI layouts

--- app/src/main/java/com/fappslab/bmi/MainActivity.kt
+++ app/src/main/java/com/fappslab/bmi/MainActivity.kt
@@ -2,10 +2,13 @@ package com.fappslab.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClickMain(view: View) {}
 }
